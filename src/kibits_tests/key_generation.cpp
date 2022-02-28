2fcc7495dbf3cd5f8d8df5146e546d641e75c44c
Lachlan Orr <lorr@gaen.org>
2022-02-27 22:57:36 -0600

    Cleaned up include and lib references in cmake files
    - Using INTERFACE properly for sodium include dirs and lib
    - Added some sample libsodium code to test
    - Validated compilation and linking of libsodium, proto files, and
      googletest

--- src/kibits_tests/key_generation.cpp
+++ src/kibits_tests/key_generation.cpp
@@ -26,7 +26,39 @@
 
 #include <gtest/gtest.h>
 
+#include <sodium.h>
+
+#include "proto/kibits/key.pb.h"
+
 TEST(GenerateKey, BasicAssertions)
 {
+    kibits::Key key;
+
+    const unsigned char * MESSAGE = (const unsigned char *) "test";
+    const size_t MESSAGE_LEN = 4;
+    const size_t CIPHERTEXT_LEN = (crypto_box_MACBYTES + MESSAGE_LEN);
+
+    unsigned char alice_publickey[crypto_box_PUBLICKEYBYTES];
+    unsigned char alice_secretkey[crypto_box_SECRETKEYBYTES];
+    crypto_box_keypair(alice_publickey, alice_secretkey);
+
+    unsigned char bob_publickey[crypto_box_PUBLICKEYBYTES];
+    unsigned char bob_secretkey[crypto_box_SECRETKEYBYTES];
+    crypto_box_keypair(bob_publickey, bob_secretkey);
+
+    unsigned char nonce[crypto_box_NONCEBYTES];
+    unsigned char ciphertext[CIPHERTEXT_LEN];
+    randombytes_buf(nonce, sizeof nonce);
+    if (crypto_box_easy(ciphertext, MESSAGE, MESSAGE_LEN, nonce,
+        bob_publickey, alice_secretkey) != 0) {
+        /* error */
+    }
+
+    unsigned char decrypted[MESSAGE_LEN];
+    if (crypto_box_open_easy(decrypted, ciphertext, CIPHERTEXT_LEN, nonce,
+        alice_publickey, bob_secretkey) != 0) {
+        /* message for Bob pretending to be from Alice has been forged! */
+    }
+
     EXPECT_EQ(7* 6, 42);
 }
